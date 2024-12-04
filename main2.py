4b26a9c2074df16d4191d014064759b3b76673c3
ExtymAndriy <150171957+ExtymAndriy@users.noreply.github.com>
2024-12-04 22:26:38 +0200

    1

--- main2.py
+++ main2.py
@@ -1,14 +1,14 @@
 import re
+from typing import Callable
 
 def generator_numbers(text):
     numbers = re.findall(r'\b\d+(?:\.\d+)?\b', text)
     for number in numbers:
         yield float(number)  
 
+def sum_profit(text: str, func: Callable[[str], float]) -> float:
+    return sum(func(text))
 
-def sum_profit(text):
-    return sum(generator_numbers(text))
-
-text = "У нас є прибуток 100.50 і витрати 50.25, а також додатковий дохід 200"
-total_profit = sum_profit(text)
-print(f"Загальний прибуток: {total_profit}")
+text = "The profits are 167, 236 and 351."
+total_profit = sum_profit(text, generator_numbers)
+print(total_profit)
